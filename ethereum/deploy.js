cfeeb3343558754111373a778060c19bdd62ef92
Marco Aurélio Prado <marco.pdsv@gmail.com>
2018-10-04 00:25:02 -0300

    Setting gas price to 10GWei

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -21,7 +21,7 @@ const deploy = async () => {
     JSON.parse(compiledFactory.interface)
   )
     .deploy({ data: compiledFactory.bytecode })
-    .send({ gas: '3000000', from: accounts[0]});
+    .send({ gas: '3000000', from: accounts[0], gasPrice: "10000000000"});
 
   console.log('Contract deployed to', result.options.address);
 
